9df28b31aad16f6046f358a935133a3fd8433d19
Rama <rmdhnprmd@gmail.com>
2024-01-11 17:29:07 +0700

    create function for small navigation

---
+++ script/script.js
@@ -0,0 +1,27 @@
+// set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+
+yearEl.textContent = currentYear;
+
+// make mobile navigation work
+
+
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight == 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported)
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+
+checkFlexGap()

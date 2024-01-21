837b1946b8dade8a5c54f5702a7972789e298040
Rama <rmdhnprmd@gmail.com>
2024-01-21 20:28:53 +0700

    fix: image optimization

--- README.md
+++
@@ -1,4 +0,0 @@
-## omnifood
-
-Web of company profile about diet food
-Built in HTML, Native CSS & Javascript Vanilla
--- script/featured-in-slide.js
+++
@@ -1,7 +0,0 @@
-function copyLogos(){
-  let copy = document.querySelector(".logos-slide").cloneNode(true);
-  document.querySelector(".logos").appendChild(copy);
-}
-copyLogos();
-copyLogos();
-copyLogos();

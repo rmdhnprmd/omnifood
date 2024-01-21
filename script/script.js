221ba8187f9715df959f88083d043bd4963c093c
Rama <rmdhnprmd@gmail.com>
2024-01-21 15:34:50 +0700

    feat: create media query for all devices & sticky nav

--- script/script.js
+++ script/script.js
@@ -5,7 +5,48 @@ const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
 // make mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
 
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    // close mobile nav
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+// Sticky
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px"
+  }
+);
+obs.observe(sectionHeroEl);
 
 function checkFlexGap() {
   var flex = document.createElement("div");
@@ -19,9 +60,9 @@ function checkFlexGap() {
   document.body.appendChild(flex);
   var isSupported = flex.scrollHeight == 1;
   flex.parentNode.removeChild(flex);
-  console.log(isSupported)
+  // console.log(isSupported)
 
   if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
 
-checkFlexGap()
+checkFlexGap();
